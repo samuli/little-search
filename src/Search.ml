5531fc6b382b525ce4db27b371984b17a9b39834
Samuli Sillanpää <samuli.sillanpaa@gmail.com>
2018-10-01 07:41:31 +0300

    Visited records.

--- src/Search.ml
+++ src/Search.ml
@@ -11,15 +11,20 @@ type msg =
   | Search
   | OnChange of string
   | GotResults of (string, string Http.error) Result.t
+  | ShowRecord of Finna.record
 [@@bs.deriving {accessors}]
 
 type model = {
     lookfor: string;
     result: Finna.searchResult remoteData;
-    cnt: int
+    visitedRecords: Finna.record array
   }
 
-let init = { lookfor = "start"; result = NotAsked; cnt = 0 }
+let init = {
+    lookfor = "start";
+    result = NotAsked;
+    visitedRecords = [||]
+  }
 
 let update model = function
   | OnSearch -> ( model, Router.openUrl (Router.routeToUrl (Search model.lookfor)))
@@ -32,13 +37,22 @@ let update model = function
      let result = Finna.decodeSearchResults data in
      ( { model with result }, Cmd.none )
   | GotResults (Error e) -> ( { model with result = Error (Http.string_of_error e) }, Cmd.none )
+  | ShowRecord r ->
+     let cmd = Router.openRoute (Record r.id) in
+     let visitedRecords = Array.append model.visitedRecords [| r |] in
+     ( { model with visitedRecords }, cmd )
+                     
+let recordItem visitedRecords r =
+  let visited =
+    begin try
+        let _el = List.find (fun el -> el.id = r.id) (Array.to_list visitedRecords) in
+        true
+      with Not_found -> false
+    end in
+  div [ classList [ "visited", visited ] ] [ a [ onClick (ShowRecord r) ] [ text r.title ] ]
 
-let recordItem r =
-  let url = Router.routeToUrl (Record r.id) in
-  div [] [ a [ href url ] [ text r.title ] ]
-
-let resultList records =
-  let items = Array.map recordItem records |> Array.to_list in
+let resultList records model =
+  let items = Array.map (recordItem model.visitedRecords) records |> Array.to_list in
   div [] items
 
 let view model =
@@ -54,9 +68,10 @@ let view model =
               ; onClick onSearch
               ; value "Search!"
          ] []
+     ; p [] [ text ("history: " ^ (String.concat " - " (Array.to_list (Array.map (fun r -> r.id) model.visitedRecords)))) ]
     ; match model.result with
       | NotAsked -> status "not asked"
       | Loading -> status "loading"
       | Error e -> status ("error: " ^ e)
-      | Success res -> resultList res.records
+      | Success res -> resultList res.records model
     ]
